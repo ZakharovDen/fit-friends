cdd7b5cc298f980df0581a47aec0cd30272dfe86
Zakharov Denis <zdv.work@yandex.ru>
2025-03-06 10:26:14 +0600

    add swagger

--- backend/apps/account/src/main.ts
+++ backend/apps/account/src/main.ts
@@ -6,14 +6,25 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+const SPEC_PATH = 'spec';
+const GLOBAL_PREFIX = 'api';
 
 async function bootstrap() {
+  const config = new DocumentBuilder()
+    .setTitle('The «Account» service')
+    .setDescription('Account service API')
+    .setVersion('1.0')
+    .build();
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(SPEC_PATH, app, document);
+  app.setGlobalPrefix(GLOBAL_PREFIX);
   const port = process.env.PORT || 3000;
   await app.listen(port);
-  Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
+  Logger.log(`🚀 Application is running on: http://localhost:${port}/${GLOBAL_PREFIX}`);
+  Logger.log(`API specification is running on: http://localhost:${port}/${SPEC_PATH}`);
 }
 
 bootstrap();
