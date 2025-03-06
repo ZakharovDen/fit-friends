24674b689d73ddc243fd438074ffda62936311f6
Zakharov Denis <zdv.work@yandex.ru>
2025-03-06 15:36:42 +0600

    helpers and interfaces

--- backend/libs/shared/core/src/index.ts
+++ backend/libs/shared/core/src/index.ts
@@ -1,1 +1,5 @@
-export * from './lib/core';
+export { User } from './lib/types/user/user.interface';
+export { AuthUser } from './lib/types/user/auth-user.interface';
+export { UserSex } from './lib/types/user/user-sex.enum';
+export { File } from './lib/types/file.interface';
+export { TokenPayload } from './lib/interfaces/token-payload.interface';
