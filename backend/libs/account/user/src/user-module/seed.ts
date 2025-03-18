7c057d1f5555c5097a28e595e05ea5463c8baf19
Zakharov Denis <zdv.work@yandex.ru>
2025-03-18 11:58:48 +0600

    допинал сиды

--- backend/libs/account/user/src/user-module/seed.ts
+++ backend/libs/account/user/src/user-module/seed.ts
@@ -13,26 +13,26 @@ const MOCK_USERS = [
     name: 'user',
     avatar: '',
     password: 'password123',
-    sex: 'Male',
+    sex: Sex.Male,
     dateOfBirth: new Date('07.09.1989'),
     description: 'Описалово',
-    location: 'Петроградская',
+    location: UserLocation.Petrogradskaya,
     backgroundImage: 'backend\\uploads\\2025\\03\\11aa6d8d-3ebd-4dda-82ca-6012a5709db7.png',
-    role: 'admin'
+    role: UserRole.Admin
   },
-  // {
-  //   id: '6581762309c030b503e30512',
-  //   email: 'user2@local.local',
-  //   name: 'user2',
-  //   avatar: '',
-  //   password: 'passwordqwer',
-  //   sex: Sex.Female,
-  //   dateOfBirth: new Date('01.01.2001'),
-  //   description: 'Описалово qqq',
-  //   location: UserLocation.Pionerskaya,
-  //   backgroundImage: 'backend\\uploads\\2025\\03\\11aa6d8d-3ebd-4dda-82ca-6012a5709db7.png',
-  //   role: UserRole.User
-  // },
+  {
+    id: '6581762309c030b503e30512',
+    email: 'user2@local.local',
+    name: 'user2',
+    avatar: '',
+    password: 'passwordqwer',
+    sex: Sex.Female,
+    dateOfBirth: new Date('01.01.2001'),
+    description: 'Описалово qqq',
+    location: UserLocation.Pionerskaya,
+    backgroundImage: 'backend\\uploads\\2025\\03\\11aa6d8d-3ebd-4dda-82ca-6012a5709db7.png',
+    role: UserRole.User
+  },
 ] as const;
 
 const UserEntity =
@@ -58,7 +58,19 @@ async function bootstrap() {
   for (const mockUser of MOCK_USERS) {
     const { id: _id, email, name, password, avatar, backgroundImage, dateOfBirth, description, location, role, sex } = mockUser;
     const passwordHash = await hash(password, salt);
-    await new UserEntity({ _id, email, name, avatar, passwordHash, backgroundImage, dateOfBirth, description, location, role, sex }).save();
+    await new UserEntity({ 
+      _id, 
+      email, 
+      name, 
+      avatar, 
+      passwordHash, 
+      backgroundImage, 
+      dateOfBirth, 
+      description, 
+      location, 
+      role, 
+      sex 
+    }).save();
   }
 
   await mongoose.disconnect?.();
