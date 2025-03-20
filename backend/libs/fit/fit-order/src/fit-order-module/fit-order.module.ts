0b8394ffb9f66ffdde1e0398a46f908be45e7dd9
Zakharov Denis <zdv.work@yandex.ru>
2025-03-20 16:23:43 +0600

    fix

--- backend/libs/fit/fit-order/src/fit-order-module/fit-order.module.ts
+++ backend/libs/fit/fit-order/src/fit-order-module/fit-order.module.ts
@@ -5,9 +5,10 @@ import { FitOrderFactory } from './fit-order.factory';
 import { FitOrderRepository } from './fit-order.repository';
 import { FitOrderService } from './fit-order.service';
 import { FitOrderEntity } from './fit-order.entity';
+import { FitTrainingModule } from '@backend/fit-training';
 
 @Module({
-  imports: [PrismaClientModule],
+  imports: [PrismaClientModule, FitTrainingModule],
   controllers: [FitOrderController],
   providers: [FitOrderFactory, FitOrderRepository, FitOrderService, FitOrderEntity],
   exports: [FitOrderService],
