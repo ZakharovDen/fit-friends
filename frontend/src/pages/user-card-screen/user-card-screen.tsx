5b6723b2da2ad82c795a67d565ab8162628dd336
Zakharov Denis <zdv.work@yandex.ru>
2025-05-29 15:22:15 +0600

    status in card

--- frontend/src/pages/user-card-screen/user-card-screen.tsx
+++ frontend/src/pages/user-card-screen/user-card-screen.tsx
@@ -32,6 +32,20 @@ function UserCardScreen(): JSX.Element {
   const { entities } = useAppSelector(getTrainings);
   //const classSuffix = (userInfo?.role === UserRole.Coach) ? '-coach' : '';
   const classSuffix = '-coach';
+  const coachStatus = <div className={`user-card${classSuffix}__status user-card${classSuffix}__status--tag`}>
+    <svg className={`user-card${classSuffix}__icon-cup`} width="12" height="13" aria-hidden="true">
+      <use xlinkHref="#icon-cup"></use>
+    </svg><span>Тренер</span>
+  </div>
+  {
+    userInfo?.questionnaire?.isReady
+    ? <div className={`user-card-coach__status user-card-coach__status--check`}><span>Готов тренировать</span></div>
+    : <div className={`user-card-coach-2__status user-card-coach-2__status--check`}><span>Не готов тренировать</span></div>
+  };
+  const sportsmanStatus = userInfo?.questionnaire?.isReady
+    ? <div className="thumbnail-friend__ready-status thumbnail-friend__ready-status--is-ready"><span>Готов к тренировке</span></div>
+    : <div className="thumbnail-friend__ready-status thumbnail-friend__ready-status--is-not-ready"><span>Не готов к тренировке</span></div>;
+
   return (
     <main>
       <div className="inner-page inner-page--no-sidebar">
@@ -53,17 +67,7 @@ function UserCardScreen(): JSX.Element {
                         </svg><span>{userInfo?.location && UserLocationLabel[userInfo?.location]}</span></a>
                       </div>
                       <div className={`user-card${classSuffix}__status-container`}>
-                        <div className={`user-card${classSuffix}__status user-card${classSuffix}__status--tag`}>
-                          <svg className={`user-card${classSuffix}__icon-cup`} width="12" height="13" aria-hidden="true">
-                            <use xlinkHref="#icon-cup"></use>
-                          </svg><span>Тренер</span>
-                        </div>
-                        {userInfo?.questionnaire?.isReady
-                          ? <div className={`user-card-coach__status user-card-coach__status--check`}><span>Готов тренировать</span></div>
-                          : <div className={`user-card-coach-2__status user-card-coach-2__status--check`}><span>Не готов тренировать</span></div>
-                        }
-                        <div className="thumbnail-friend__ready-status thumbnail-friend__ready-status--is-ready"><span>Готов к тренировке</span></div>
-                        <div className="thumbnail-friend__ready-status thumbnail-friend__ready-status--is-not-ready"><span>Не готов к тренировке</span></div>
+                        {(userInfo?.role === UserRole.Coach) ? coachStatus : sportsmanStatus}
                       </div>
                       <div className={`user-card${classSuffix}__text`}>
                         {userInfo?.description}
@@ -93,21 +97,21 @@ function UserCardScreen(): JSX.Element {
                     <div className={`user-card${classSuffix}__gallary`}>
                       <ul className={`user-card${classSuffix}__gallary-list`}>
                         <li className={`user-card${classSuffix}__gallary-item`}>
-                          <img 
-                            src="img/content/user-coach-photo1.jpg" 
-                            srcSet="img/content/user-coach-photo1@2x.jpg 2x" 
-                            width="334" 
-                            height="573" 
-                            alt="photo1" 
+                          <img
+                            src="img/content/user-coach-photo1.jpg"
+                            srcSet="img/content/user-coach-photo1@2x.jpg 2x"
+                            width="334"
+                            height="573"
+                            alt="photo1"
                           />
                         </li>
                         <li className={`user-card${classSuffix}__gallary-item`}>
-                          <img 
-                            src="img/content/user-coach-photo2.jpg" 
-                            srcSet="img/content/user-coach-photo2@2x.jpg 2x" 
-                            width="334" 
-                            height="573" 
-                            alt="photo2" 
+                          <img
+                            src="img/content/user-coach-photo2.jpg"
+                            srcSet="img/content/user-coach-photo2@2x.jpg 2x"
+                            width="334"
+                            height="573"
+                            alt="photo2"
                           />
                         </li>
                       </ul>
